a207007f8637983b46596806eea431236244cd2b
luissaenz-git <lsaenz@byupathway.edu>
2025-07-06 17:29:28 -0400

    Exercises 1 to 3 from the first week

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string scale = "";
+        string status = "";
+        //after the variable is entered we make an operation to know the last digit
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            scale = "A";
+        }
+        else if (grade >= 80)
+        {
+            scale = "B";
+        }
+        else if (grade >= 70)
+        {
+            scale = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            scale = "D";
+        }
+        else
+        {
+            scale = "F";
+        }
+
+        if (grade >= 70)
+        {
+            status = "Good work! Keep going!";
+        }
+        else
+        {
+            status = "Try your best next time!";
+        }
+        // we use double equal == instead of in due to C# does not contain the in operator
+        if (scale == "B" || scale == "C" || scale == "D" )
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (scale == "A")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        Console.WriteLine($"Your grade in the scale is {scale}{sign}.");
+        Console.WriteLine();
+        Console.WriteLine($"{status}");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randonGenerator = new Random(); // CreateS a random number generator
+
+        string playAgain = "yes";
+
+
+        while (playAgain == "yes")
+        {
+            int number = randonGenerator.Next(1, 100);
+            int counter = 0;
+            int guessNumber = 0;
+
+            Console.WriteLine($"What is the magic number? {number}");
+
+            int magicNumber = number;
+
+            do //If the user guesses the number, the loop will stop, even in the first try
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();   //not needed to declare again the data type later
+                guessNumber = int.Parse(userInput);
+                counter++; //Increment the counter plus 1 for each guess
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it after {counter} tries!");
+                }
+
+            } while (guessNumber != magicNumber) ;
+
+                Console.Write("Do you want to play again? (yes/no) ");
+                playAgain = Console.ReadLine().ToLower(); // Convert input to lowercase for consistency
+
+            }
+            Console.WriteLine("Thanks for playing!");
     }
 }
+
+
